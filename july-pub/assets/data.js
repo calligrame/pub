01c4fd5c54be951be2ea4dbda49bce03e4e285a4
calligrame <calli119119@gmail.com>
2025-07-21 10:12:28 +0900

    7월호 데모

--- july-pub/assets/data.js
+++ july-pub/assets/data.js
@@ -24,7 +24,7 @@ var eBookData = {
 	pagearea:true,
 	pagesound:"sound1",
 	intro:"basic",
-	RANDOM:609696,
+	RANDOM:529174,
 	
 	
 	bookList : [ /** 이전호 목록 */
@@ -113,7 +113,8 @@ var eBookData = {
 ],
 [83,{type:"image", id:"Image_26", name:"noname", visible:"true", x:"1048", y:"702", width:"435", height:"170", opacity:"100", normalImage:"1752111259116.png", rolloverImage:"", link:"address|_blank|http://pf.kakao.com/_xcuXxcb"}
 ],
-[84,{type:"image", id:"Image_31", name:"", visible:"true", x:"164", y:"1222", width:"423", height:"205", opacity:"100", normalImage:"1752806517034.png", rolloverImage:"", link:"address|_blank|https://forms.gle/WvHgAwnZxh6GFNqCA"}
+[84,{type:"image", id:"Image_31", name:"", visible:"true", x:"189", y:"1167", width:"309", height:"205", opacity:"100", normalImage:"1752806517034.png", rolloverImage:"", link:"address|_blank|https://forms.gle/WvHgAwnZxh6GFNqCA"},
+{type:"image", id:"Image_32", name:"", visible:"true", x:"694", y:"1177", width:"323", height:"176", opacity:"100", normalImage:"1753060080848.png", rolloverImage:"", link:"address|_blank|https://forms.gle/46W8PT7CNADjxnMM7"}
 ],
 [86,{type:"image", id:"Image_27", name:"noname", visible:"true", x:"680", y:"2214", width:"312", height:"202", opacity:"100", normalImage:"1752111233778.png", rolloverImage:"", link:"address|_blank|https://www.jdcenter.com/main.cs"}
 ]],
